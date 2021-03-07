75eae165f37a7a63d2f1520ba7b1541c9269a6cb
gagandeep.ahuja <gagandeepahuja09@gmail.com>
2021-03-07 13:04:16 +0530

    access modifiers in classes

--- features/classes.ts
+++ features/classes.ts
@@ -2,26 +2,31 @@
 // we create an instance of it.
 
 class Vehicle {
-  drive(): void {
-    console.log('Chugga Chugga')
-  }
-
-  honk(): void {
+  protected honk(): void {
     console.log('beep')
   }
 }
 
 // when we are extending a class, we can choose to override some of the methods
+// default behaviour for classes in JS is public
+
+// we can't modify the access modifiers of the parent class in the child class.
+
+// by adding private, we are not adding any layer of application security
+// we use private methods, if we feel that providing access to some methods or properties
+// may lead to other developers accidentally breaking the application / code.
 class Car extends Vehicle {
-  drive(): void {
+  private drive(): void {
     console.log('Vrooooom.....')
+  }
+  protected honk(): void {
+    console.log('beep beep')
+  }
+  startDrivingProcess(): void {
+    this.drive()
+    this.honk()
   }
 }
 
-const vehicle = new Vehicle
-vehicle.drive()
-vehicle.honk()
-
 const car = new Car()
-car.drive()
-car.honk()
+car.startDrivingProcess()

954584aafaa776b2e5673c9328aeae4196b9491b
gagandeep.ahuja <gagandeepahuja09@gmail.com>
2021-05-08 18:06:15 +0530

    reading csv file

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('Hi there');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// we will need to install type definition file for standard node packages as well
+// npm i @types/node
+var fs_1 = __importDefault(require("fs"));
+var matches = fs_1.default
+    .readFileSync('football.csv', {
+    encoding: 'utf-8'
+})
+    .split('\n')
+    .map(function (row) { return row.split(','); });
+console.log('Hi there', matches);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hi there')
+// we will need to install type definition file for standard node packages as well
+// npm i @types/node
+import fs from 'fs'
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8'
+  })
+  .split('\n')
+  .map((row: string): string[] => row.split(','))
+
+console.log('Hi there', matches)

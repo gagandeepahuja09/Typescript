8a43b025b3f75cab9047c2ba48fbd9a22f78b3c9
gagandeep.ahuja <gagandeepahuja09@gmail.com>
2021-05-14 18:34:46 +0530

    generic constraints

--- features/generics.ts
+++ features/generics.ts
@@ -22,3 +22,43 @@ const printArray = <T>(arr: T[]): void => {
 }
 
 printArray<string | number>(['a', 'b', 'c', 1])
+
+// Generic Constraints
+// Feature to limit the types that we can pass through
+
+class House {
+  print(): void {
+    console.log('This is a house')
+  }
+}
+
+class Car {
+  print(): void {
+    console.log('This is a car')
+  }
+}
+
+// Without constraints
+// const printHousesOrCars = <T>(arr: T[]): void => {
+//   for (let i = 0; i < arr.length; i++) {
+//     // we get an error ==> Property print does not exist on type T
+//     // this is because here T can be any possible type
+//     // Hence we need to put a constraint on it
+//     arr[i].print()
+//   }
+// }
+
+interface Printable {
+  print(): void
+}
+
+const printHousesOrCars = <T extends Printable>(arr: T[]): void => {
+  for (let i = 0; i < arr.length; i++) {
+    // we get an error ==> Property print does not exist on type T
+    // this is because here T can be any possible type
+    // Hence we need to put a constraint on it
+    arr[i].print()
+  }
+}
+
+printHousesOrCars<House>([new House(), new House()])

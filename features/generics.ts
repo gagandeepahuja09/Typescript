65c11f79003852f25b9686e7ecb67634b444afff
gagandeep.ahuja <gagandeepahuja09@gmail.com>
2021-05-14 17:53:21 +0530

    generics in functions

--- features/generics.ts
+++ features/generics.ts
@@ -6,9 +6,19 @@ class ArrayOfAnything<T> {
 }
 // Just a like a function(parameters) 
 // We don't know the type we can expect at runtime. 
-
 new ArrayOfAnything<string>(['a', 'b', 'c'])
 
 // Type inference with Generics
 // Ts will figure out the type on it's own if we don't tell it.
+// This is very much similar to type inference for return types.
+// Here also, we should manually add the type because it will help
+// us detect errors.
 new ArrayOfAnything(['a', 'b', 'c', 1, true])
+
+const printArray = <T>(arr: T[]): void => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+  }
+}
+
+printArray<string | number>(['a', 'b', 'c', 1])

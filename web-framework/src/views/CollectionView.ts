5690c63993f787ed3339f89020d3372399a59a64
gagandeep.ahuja <gagandeepahuja09@gmail.com>
2021-06-01 21:46:45 +0530

    implement render method in collection view

--- web-framework/src/views/CollectionView.ts
+++ web-framework/src/views/CollectionView.ts
@@ -3,8 +3,19 @@ import { Collection } from '../models/Collection';
 export abstract class CollectionView<T, K> {
   constructor(public parent: Element, public collection: Collection<T, K>) {
   }
-  abstract renderItem(): void
-  render(): void {
+  abstract renderItem(model: T, itemParent: Element): void
 
+  render(): void {
+    this.parent.innerHTML = ''
+
+    const templateElement = document.createElement('template')
+
+    for (let model of this.collection.models) {
+      const itemParent = document.createElement('div')
+      this.renderItem(model, itemParent)
+      templateElement.append(itemParent)
+    }
+
+    this.parent.append(templateElement.content)
   }
 }
